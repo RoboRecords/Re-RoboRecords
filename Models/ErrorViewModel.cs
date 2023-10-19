9cf5035a5a7542d7fbce105602055b90953d744c
Blake <lemindevelopment@gmail.com>
2023-10-19 21:55:34 +0100

    Homepage and Postgre Setup
    Basic homepage design

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ReRoboRecords.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
