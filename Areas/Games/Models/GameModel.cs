21fad1d3d6c418b9c1eb65f22fb23e9b6c2bb754
Lemin <blake@eversome.net>
2023-11-08 13:23:55 +0000

    Add Game link on Games page
    Also improved spacing on the GameModel.cs

--- Areas/Games/Models/GameModel.cs
+++ Areas/Games/Models/GameModel.cs
@@ -12,23 +12,28 @@ namespace ReRoboRecords.Areas.Games.Models
         /// Id of the Game.
         /// </summary>
         public int Id { get; set; }
+        
         /// <summary>
         /// Name of the Game.
         /// </summary>
         [Required]
         public string Name { get; set; }
+        
         /// <summary>
         /// Description of the Game.
         /// </summary>
         public string Description { get; set;}
+        
         /// <summary>
         /// Release date of the Game.
         /// </summary>
         public string ReleaseDate { get; set; }
+        
         /// <summary>
         /// Privacy state. If private, should be hidden to non-admins.
         /// </summary>
         public bool IsPrivate { get; set; }
+        
         /// <summary>
         /// Categories of the game.
         /// </summary>
