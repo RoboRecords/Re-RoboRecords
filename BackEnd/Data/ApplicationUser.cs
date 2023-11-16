03875e711cc1cf90d155f394e1245fd7868bfb96
Lemin <blake@eversome.net>
2023-11-16 20:50:09 +0000

    Initial Blazor Back end & front end template set up
    Trying out Blazor on a seperate branch because I just really want to see how beneficial this could be.

---
+++ BackEnd/Data/ApplicationUser.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace BackEnd.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+}
