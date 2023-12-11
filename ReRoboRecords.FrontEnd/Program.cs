e6cd66e475e836c5cbfe73eb21d9be09df378577
Lemin <blake@eversome.net>
2023-12-11 22:47:01 +0000

    SupaBase Login functionality & shared library
    + Can now log into users already registered onto the SupaBase auth. The user gets a JWT token from it.

--- ReRoboRecords.FrontEnd/Program.cs
+++ ReRoboRecords.FrontEnd/Program.cs
@@ -1,11 +1,51 @@
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ReRoboRecords.FrontEnd;
+using ReRoboRecords.FrontEnd.Providers;
+using ReRoboRecords.FrontEnd.Services;
+using Supabase.Gotrue;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddBlazoredLocalStorage();
+
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>(
+    provider => new CustomAuthStateProvider(
+        provider.GetRequiredService<ILocalStorageService>(),
+        provider.GetRequiredService<Supabase.Client>(),
+        provider.GetRequiredService<ILogger<CustomAuthStateProvider>>()
+    )
+);
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddAuthorizationCore();
+
+var url = "url";
+var key = "key";
+
+
+builder.Services.AddScoped<Supabase.Client>(
+    provider => new Supabase.Client(
+        url,
+        key,
+        new Supabase.SupabaseOptions
+        {
+            AutoRefreshToken = true,
+            AutoConnectRealtime = true,
+            SessionHandler = new CustomSupabaseSessionHandler(
+                provider.GetRequiredService<ILocalStorageService>(),
+                provider.GetRequiredService<ILogger<CustomSupabaseSessionHandler>>()
+            )
+        }
+    )
+);
+
+builder.Services.AddScoped<AuthService>();
+
 
 await builder.Build().RunAsync();
---
+++ ReRoboRecords.Shared/UserModel.cs
@@ -0,0 +1,11 @@
+
+namespace ReRoboRecords.Shared;
+
+public class UserModel 
+{
+    public  string UserName { get; set; }
+
+    public  string Email { get; set; }
+    
+    public  string Password { get; set; }
+}
