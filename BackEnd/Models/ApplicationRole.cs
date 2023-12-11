131d67d3003a9286895399a157a298debbb4ea52
Lemin <blake@eversome.net>
2023-12-11 19:58:32 +0000

    Remove old backend & frontend

--- BackEnd/Models/ApplicationRole.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace BackEnd.Models;
-
-public class ApplicationRole : IdentityRole
-{
-    
-}
--- BackEnd/Models/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-using BackEnd.Models;
-using Microsoft.AspNetCore.Identity;
-
-namespace BackEnd.Data;
-
-public class ApplicationUser : IdentityUser
-{
-    public virtual ICollection<Run> Runs { get; set; }
-}
