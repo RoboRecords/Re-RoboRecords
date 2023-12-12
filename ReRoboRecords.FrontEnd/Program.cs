e82cd27544cccab5c1441287853ab3d1a7a3ea36
Lemin <blake@eversome.net>
2023-12-12 11:44:31 +0000

    Config Setup

--- ReRoboRecords.FrontEnd/Program.cs
+++ ReRoboRecords.FrontEnd/Program.cs
@@ -2,9 +2,11 @@ using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
 using ReRoboRecords.FrontEnd;
 using ReRoboRecords.FrontEnd.Providers;
 using ReRoboRecords.FrontEnd.Services;
+
 using Supabase.Gotrue;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -12,6 +14,12 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+var config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+
+builder.Configuration.AddConfiguration(config);
 
 builder.Services.AddBlazoredLocalStorage();
 
@@ -25,9 +33,8 @@ builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddAuthorizationCore();
 
-var url = "url";
-var key = "key";
-
+var url = builder.Configuration["ApiSettings:BaseUrl"];
+var key = builder.Configuration["ApiSettings:ApiKey"];
 
 builder.Services.AddScoped<Supabase.Client>(
     provider => new Supabase.Client(
